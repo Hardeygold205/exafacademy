20fb5352e3f920e14619ffcee3086320468b05f1
Surajudeen Hadi Ademola <138166910+Hardeygold205@users.noreply.github.com>
2026-02-03 11:52:41 +0100

    added suspense to verify email page

--- app/verify-email/page.tsx
+++ app/verify-email/page.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { Suspense } from "react";
 import { Metadata } from "next";
-import VerifyEmail from '@/components/VerifyEmail';
+import VerifyEmail from "@/components/VerifyEmail";
 
 export const metadata: Metadata = {
   title: "Verify Your Email",
 };
 
 function Verify() {
-    
   return (
-   <VerifyEmail />
+    <Suspense>
+      <VerifyEmail />
+    </Suspense>
   );
 }
 
